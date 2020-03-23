cff242f8450f3dc12f42a0db2964440f66620fb7
Kyungwook <Cho>
2020-03-23 17:20:35 +0900

    add Counter

---
+++ src/components/Supermarket.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SupermarketTemplate from './SupermarketTemplate';
+
+const Supermarket = () => {
+    return <SupermarketTemplate items={null} basket={null} />;
+};
+
+export default Supermarket;
